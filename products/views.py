d7dbe3c9f96ff5aeabce88c74bdeb1610586facf
Pankaj Gupta <pankajgupta@MacBook-Air.local>
2024-06-15 12:37:39 +0530

    fix product page

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, ProductImage
 
 def product(request, slug):
   try:
     product = Product.objects.get(slug = slug)
+    product_images = ProductImage.objects.filter(product_id = product.uid)
+    print(product_images[0].image)
     context = {
       'product':product,
-      'user':request.user
+      'user':request.user,
+      'selected_size': '',
+      'selected_img': product_images[0],
+      'updated_price': None
     }
 
     if request.GET.get('size'):
@@ -14,6 +19,10 @@ def product(request, slug):
       price = product.get_price_by_size(size)
       context['selected_size'] = size
       context['updated_price'] = price
+
+    if request.GET.get('img'):
+      img = request.GET.get('img')
+      context['selected_img'] = ProductImage.objects.get(uid=img)
 
     return render(request, 'product/product.html', context)
   
